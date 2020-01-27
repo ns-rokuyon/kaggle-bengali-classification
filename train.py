ddd8de54ecb0098fa5558c4c04365978c9a4fd54
ns-rokuyon <ness3psi@gmail.com>
2020-01-27 19:46:37 +0900

    add command line args

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
+import torchvision
+from torch.utils.data.dataloader import DataLoader
 from pathlib import Path
 from argparse import ArgumentParser
+
 from dataset import bengali_dataset
 from data import get_logger
 
@@ -10,6 +13,10 @@ def parse_args():
                         help='Path to data dir')
     parser.add_argument('--fold-id', default=0, type=int,
                         help='Kfold id')
+    parser.add_argument('--n-epoch', default=30, type=int,
+                        help='Number of epoch')
+    parser.add_argument('--arch', default='resnet34',
+                        help='Networks arch')
     return parser.parse_args()
 
 
@@ -23,6 +30,20 @@ def main():
                                                  fold_id=fold_id,
                                                  logger=logger)
     logger.info(f'#train={len(train_dataset)}, #val={len(val_dataset)}')
+
+    train_loader = DataLoader(train_dataset, num_workers=8,
+                              pin_memory=True, drop_last=True)
+    val_loader = DataLoader(train_dataset, num_workers=8,
+                            pin_memory=True, drop_last=True)
+
+    train(train_loader, val_loader,
+          n_epoch=args.n_epoch)
+
+
+def train(train_loader, val_loader,
+          n_epoch=30):
+    pass
+
 
 
 if __name__ == '__main__':
