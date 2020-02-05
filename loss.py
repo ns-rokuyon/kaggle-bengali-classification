36b421843a49a748a84eeee54719f62f1c62546e
ns-rokuyon <ness3psi@gmail.com>
2020-02-05 22:20:36 +0900

    add loss module

---
+++ loss.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_criterion(loss_type, **kwargs):
+    if loss_type == 'ce':
+        return F.cross_entropy
+    elif loss_type == 'ohem':
+        return OHEMCrossEntropyLoss(**kwargs).cuda()
+    else:
+        raise ValueError(loss_type)
+
+
+class OHEMCrossEntropyLoss(nn.Module):
+    def __init__(self, rate=0.7, **kwargs):
+        super().__init__()
+        self.rate = rate
+
+    def forward(self, logit, t):
+        batch_size = logit.size(0)
+        loss = F.cross_entropy(logit, t,
+                               reduction='none',
+                               ignore_index=-1)
+        loss, _ = loss.topk(k=int(self.rate * batch_size))
+        return torch.mean(loss)
